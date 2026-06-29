124539e63ac6f34b3a938298f0ce08f34754b573
CRedRiver <mutattom1309@gmail.com>
2026-06-29 23:50:06 +0700

    rename to custom router

---
+++ custom_router/router.py
@@ -0,0 +1,30 @@
+import numpy as np
+from semantic_router.encoders import FastEmbedEncoder
+
+class SemanticRouter():
+    def __init__(self, routes, 
+                 embedding=FastEmbedEncoder(name="sentence-transformers/all-MiniLM-L6-v2")):
+        self.routes = routes
+        self.embedding = embedding
+        self.routesEmbedding = {}
+
+        for route in self.routes:
+            self.routesEmbedding[
+                route.name
+            ] = self.embedding.encode(route.samples)
+
+    def get_routes(self):
+        return self.routes
+
+    def guide(self, query):
+        queryEmbedding = self.embedding.encode([query]) 
+        queryEmbedding = queryEmbedding / np.linalg.norm(queryEmbedding)
+        scores = []
+
+        for route in self.routes:
+            routesEmbedding = self.routesEmbedding[route.name] / np.linalg.norm(self.routesEmbedding[route.name])
+            score = np.mean(np.dot(routesEmbedding, queryEmbedding.T).flatten())
+            scores.append((score, route.name))
+
+        scores.sort(reverse=True)
+        return scores[0][1]
